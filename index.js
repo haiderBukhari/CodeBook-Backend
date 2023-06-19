4820019612ae831496b3c10f0904123b3524fc8c
Muhammad Haider Bukhari <85192296+haiderBukhari@users.noreply.github.com>
2023-06-19 08:28:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 import express from "express";
-import jsonServer from 'json-server'
-import auth from 'json-server-auth'
+import jsonServer from "json-server";
+import auth from "json-server-auth";
 
 const server = express();
-server.use((req, res, next)=>{
+server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', '*')
     next()
 })
-const router = jsonServer.router('./Data/db.json');
-server.use('/api', router)
-server.db =  router.db
-const middlewares = jsonServer.defaults()
 
+const router = jsonServer.router('./data/db.json');
+server.use('/api', router);
+server.db = router.db
+
+const middlewares = jsonServer.defaults()
+const rules = auth.rewriter({
+    products: 444,
+    featured_products: 444,
+    orders: 660,
+    users: 600
+});
+
+server.use(rules)
 server.use(auth)
 server.use(middlewares)
 server.use(router)
-server.listen(8000)
+
+const port = process.env.PORT || 8000;
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
